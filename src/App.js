1ca6ad5c88a5c8ef425adcf87431299ce1d33e15
BalanaguYashwanth <balanaguyashwanth@gmail.com>
2021-01-04 23:46:33 +0530

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state={
+    name:'yash',
+    age:'21',
+  }
+
+  change(e){
+    alert('done');
+  }
+
+  hovered(e){
+    console.log(e.target,e.pageX);
+  }
+
+  handlecopy(){
+   alert("please don't copy");
+  }
+
+  handleinput=()=>{
+    this.setState({
+      name:document.getElementById("name").value
+    })
+    console.log(document.getElementById("name").value)
+  }
+
+  render(){
+    return(
+      <div> 
+        <p> {this.state.name} </p>
+        <input type="text" id="name" />
+        <button onClick={this.handleinput} > submit name </button>
+        <p> hey hello man {this.state.name} </p>
+        <p> hey, your age is {this.state.age} </p>
+        <p> {Math.random()*10} </p>
+        <button onClick={this.change}> submit  </button>
+        <button onMouseOver={this.hovered} > hover </button>
+        <p onCopy={this.handlecopy} > copied alerts </p>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
