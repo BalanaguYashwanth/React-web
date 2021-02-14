191d79da06e549cdba590448112de86506368b1a
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-02-15 01:12:04 +0530

    Add files via upload

--- components/secretadmin.js
+++ components/secretadmin.js
@@ -2,43 +2,32 @@ import React, { useState } from 'react'
 
 export default function admin() {
 
-    const [fields, setFields] = useState([{fields:'enter field'},{fields:'enter field'}])
-    const [flag, setFlag] = useState(false);
+    const [fields, setFields] = useState([{ fields: 'enter field' }, { fields: 'enter field' }])
+
+    const [subfields, setSubfields] = useState({ subfields: 'enter subfields' , fields: 'enter fields' })
+
 
     function addfields() {
-            // let li=document.createElement('LI')
-            // li.appendChild(document.createTextNode('Fields'))
-            // li.appendChild(document.createElement('input'))
-            // document.getElementById("addinputfields").appendChild(li)
-            //allfields=[...fields]
-            //allfields.push({fields:''})
-            setFields([...fields,{fields:''}])
-            
+        setFields([...fields, { fields: '' }])
     }
 
+    function addsubfields() {
+        alert('correcr it')
+        //setSubfields({...subfields,  subfields: '' })
+    }
 
-   function addsubfields(){
-        let ul=document.createElement('UL')
-        document.getElementById('sub').appendChild(document.createTextNode('Subtitle'))
-        document.getElementById('sub').appendChild(document.createElement('input'))
-        let li=document.createElement('LI')
-        li.appendChild(document.createTextNode('Feilds'))
-        li.appendChild(document.createElement('input'))
-        let button = document.createElement('BUTTON')
-        button.setAttribute('onClick',"{addfields}")
-        button.appendChild(document.createTextNode('+'))
-        console.log(button)
-        li.appendChild(button)
-        ul.appendChild(li)
-        document.getElementById('sub').appendChild(ul)       
-   }
-//addfields1()
+    function handleaddfields(index, value) {
+        console.log(index, value)
+        fields[index].fields = value
+        console.log(fields)
+    }
 
-   function handleaddfields(index,value){
-       console.log(index,value) 
-       fields[index]={fields:value}
-       console.log(fields) 
-   }
+    function handleaddsubfields(index, value) {
+        var fsub = [...subfields]
+        fsub[index].subfields = value
+        console.log(fsub)
+    }
+
 
     return (
         <div>
@@ -46,29 +35,44 @@ export default function admin() {
                 <li>
                     Title
                     <ul>
-                        <li> Subtitle <input placeholder="enter subtitle" />  </li>
+                        <li>
+                            Subtitle <input placeholder="enter subtitle" />
+                            <ul>
+                                <div id="addinputfields">
+                                    <li>  Fields <input placeholder="enter field" />  </li>
+                                    {
+                                        fields.map((field, index) => (
+                                            <div key={index} > Fields <input placeholder={field.fields} onChange={(e) => handleaddfields(index, e.target.value)} /> </div>
 
-                        <ul >
-                            <div id="addinputfields">
-                            <li>  Fields <input placeholder="enter field" />  </li>
-                            {
-                                fields.map((field,index) => (
-                                    <div key={index} > <input   placeholder={field.fields} onChange={ (e) => handleaddfields(index,e.target.value)}   /> </div>
-                                    
-                                    ))
-                            } 
-                            </div>
-                            <li> <button className="fields" onClick={addfields}> + </button>  </li>
-                        </ul>
-
-                        <li> 
-                            <div id="sub">
-                                
-                            </div>
-                            <button className="sub" onClick={addsubfields} > + </button> 
+                                        ))
+                                    }
+                                </div>
+                                <li> <button className="fields" onClick={addfields}> + </button>  </li>
+                            </ul>
                         </li>
-                        
-                       
+
+                        <li>
+
+                            {/* {
+                                subfields.map((field, index) => (
+
+                                    <div key={index} > Subfields <input placeholder={field.subfields} onChange={(e) => handleaddsubfields(index, e.target.value)} /> </div>
+
+                                ))
+                            } */}
+
+                            {
+                                Object.entries(subfields).map(([key, value]) => (
+
+                                    <div key={key} > {key} {value} </div>
+
+                                ))
+                            }
+
+                            <button className="sub" onClick={addsubfields} > + </button>
+                        </li>
+
+
                     </ul>
                 </li>
             </ul>
