81582db319440197cda6dec2696e90271bc40603
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-02-22 22:39:52 +0530

    Update updatelist.js

--- components/updatelist.js
+++ components/updatelist.js
@@ -14,12 +14,26 @@ export default function updatelist() {
                 title: title,
                 imgurl: imgurl
             })
-                .then(res => console.log(res))
+                .then(res => {
+                    console.log(res)
+                    location.reload()
+                })
                 .catch(err => console.log(err))
         }
         else {
             setFeedback('Please enter the all inputs')
         }
+
+    }
+
+    function deleting(id,title){
+        
+        axios.delete('http://127.0.0.1:8000/api/courses/'+id+'/')
+        .then(res=>{
+            alert( 'Successfully deleted course :- '+title)
+            location.reload()
+        })
+        .catch(err=>err)
 
     }
 
@@ -52,13 +66,12 @@ export default function updatelist() {
                 <div className="rightside">
                     <div className="container" style={{ margin: 'auto' }}>
                         <h3> Updated Courses </h3>
-
                         {
                             items && items.map((item, index) => (
                                 <div key={index}>
                                     <div className="card bg-light mb-3" style={{ width: '18rem' }} >
                                         <div className="card-body px-5 ">
-                                            <p className="card-title "> {index+1})  {item.title}  <a href={item.imgurl}> <i className="fas fa-external-link-alt"></i>  </a> <button style={{float:'right',border:'None'}} > <i className="far fa-trash-alt"></i></button> </p>
+                                            <p className="card-title "> {index+1})  {item.title}  <a href={item.imgurl}> <i className="fas fa-external-link-alt"></i>  </a> <button style={{float:'right',border:'None'}} onClick={() => deleting(item.id,item.title) } > <i className="far fa-trash-alt"></i></button> </p>
                                         </div>
                                     </div>
                                 </div>
