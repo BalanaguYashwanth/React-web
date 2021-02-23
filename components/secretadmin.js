8b4af19d7e1eba590ee5aee94f20fa85ee2f22c2
B.yashwanth <36238382+BalanaguYashwanth@users.noreply.github.com>
2021-02-23 19:09:22 +0530

    Add files via upload

--- components/secretadmin.js
+++ components/secretadmin.js
@@ -2,89 +2,140 @@ import React, { useState } from 'react'
 
 export default function admin() {
 
-    const [fields, setFields] = useState([{ fields: 'enter field' }, { fields: 'enter field' }])
+    //const [fields, setFields] = useState([{ fields: 'enter field' }, { fields: 'enter field' }])
 
-    const [subfields, setSubfields] = useState({ subfields: 'enter subfields',fields1: 'enter fields',fields2: 'enter fields'})
+    const [subfields, setSubfields] = useState(
+        [
+            {
+                id: 1,
+                subtitle: 'enter subfields',
+                videos: [{ id: 1, field: "enter field1", link: "enter the link" }]
+            },
+            {
+                id: 2,
+                subtitle: 'enter subfields',
+                videos: [{ id: 1, field: "enter field", link: "enter the link" },{ id: 2, field: "enter field", link: "enter the link" }]
+            },
+        ]
+    )
 
+    const [title,setTitle] = useState()
 
-    function addfields() {
-        setFields([...fields, { fields: '' }])
+    //const [resubfields,setResubfields] = useState([])
+
+    function add(field) {
+        let svideo = field.videos.length
+
+        field.videos.push(
+            {
+                id:svideo+1,
+                field: "enter field", 
+                link: "enter the links", 
+            }
+        )
+        
+        setSubfields([...subfields])
+        //console.log(subfields)           //required
+        // setFields([...fields, { fields: '' }])
     }
 
     function addsubfields() {
-        alert('correct it')
-        //setSubfields({...subfields,  subfields: '' })
+        let obj = {
+            id: subfields.length + 1,
+            subtitle: 'enter subfields',
+            videos: [{ id: 1, field: "enter field", link: "enter the link" }]
+        }
+        setSubfields([...subfields, obj])
+        console.log(subfields)
     }
 
-    function handleaddfields(index, value) {
-        console.log(index, value)
-        fields[index].fields = value
-        console.log(fields)
+    // function handleaddfields(index, value) {
+    //     console.log(index, value)
+    //     fields[index].fields = value
+    //     console.log(fields)
+    // }
+
+    // function handleaddsubfields(field,value,index) {
+
+    //     subfields[index][field]=value
+    //     console.log(subfields)
+    // }
+
+
+    // function calculate(data,index) {
+    //     const list=[]
+
+    //     for (let field in data) {
+    //         list.push(<li key={field} > {field}  <input  placeholder={data[field]} onChange={(e) => (handleaddsubfields(field,e.target.value,index))} />  </li>)
+    //     }
+
+    //     return(
+    //         <div>
+    //         {list}
+    //         </div>
+    //     )
+    // }
+
+
+    function handlesubtitle(text,index){
+        subfields[index].subtitle=text
+        console.log(subfields)
     }
 
-    function handleaddsubfields(index,key,value) {
-
-        // let fsub={}
-
-        // for(let obj in subfields)
-        // {
-        //     fsub[obj]=subfields[obj]
-        // }
-
-        // //console.log(subfields)
-        // //subfields[index].key=value
-        // // let fsub = [...subfields]
-        // // fsub[index].subfields= value
-        // console.log(fsub[key])
-
-        console.log(index,key,value)
+    function handlevideos(text,index,mainindex){
+        subfields[mainindex].videos[index].field=text
+        console.log(subfields)
     }
 
+    function handlevideoslink(link,index,mainindex){
+        subfields[mainindex].videos[index].link=link
+        console.log(subfields)
+    }
+
+
+    function videoslist(valvideo,mainindex) {
+       
+        return (
+            <div>
+                {
+                    valvideo.map((val,index) => (
+                        <div key={index}>
+                        <li>  <input placeholder={val.field} onChange={(e) => handlevideos(e.target.value,index,mainindex)} />   </li>
+                        <li>  <input placeholder={val.link}  onChange={(e) => handlevideoslink(e.target.value,index,mainindex)}  />    </li>
+                        </div>
+                    ))
+                }
+            </div>
+        )
+    }
 
     return (
         <div>
             <ul>
                 <li>
-                    Title
-                    <ul>
-                        <li>
-                            Subtitle <input placeholder="enter subtitle" />
-                            <ul>
-                                <div id="addinputfields">
-                                    <li>  Fields <input placeholder="enter field" />  </li>
-                                    {
-                                        fields.map((field, index) => (
-                                            <div key={index} > Fields <input placeholder={field.fields} onChange={(e) => handleaddfields(index, e.target.value)} /> </div>
-
-                                        ))
-                                    }
-                                </div>
-                                <li> <button className="fields" onClick={addfields}> + </button>  </li>
-                            </ul>
-                        </li>
-
-                        <li>
-
-                            {
-                                Object.entries(subfields).map(([key, value],index) => (
-
-                                    <div key={key} > {key} < input placeholder={value} onChange={(e) => handleaddsubfields(index,key,e.target.value)} />   </div>
-
-                                ))
-                            }
-
-
-                            <button className="sub" onClick={addsubfields} > + </button>
-                        </li>
-
+                    Title <input placeholder="enter the title" onChange={ (e) => setTitle(e.target.value) } />
+                   <ul>
+                        {
+                            subfields.map((field, index) => (
+                                <li key={index} >
+                                   subtitle <input placeholder= {field.subtitle} onChange={ (e) => (handlesubtitle(e.target.value,index)) }  />
+                                    <ul> 
+                                       <li> 
+                                           videos 
+                                           <ul> {videoslist(field.videos,index)} </ul>
+                                           <button onClick={() => (add(field))} > + </button>
+                                        </li> 
+                                       
+                                    </ul> 
+                                </li>
+                            ))
+                        }
                     </ul>
+                    <button onClick={addsubfields} > + </button>
                 </li>
             </ul>
-
-            <button> submit </button>
         </div>
 
     )
-
 }
 
